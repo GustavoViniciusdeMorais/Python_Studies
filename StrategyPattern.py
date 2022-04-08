73fe2623f43d2564f8a2c2d1fca9dd900542eca0
Gustavo Vinicius <gustavo@appfacilita.com>
2022-04-08 09:17:23 -0300

    Better Strategy Pattern Example

--- StrategyPattern.py
+++ StrategyPattern.py
@@ -1,20 +1,67 @@
-class Route():
-    def get_path(self, a, b):
+import re
+from abc import abstractmethod
+
+'''
+Python abc library is an Abstract Base Class
+link to library https://docs.python.org/3/library/abc.html
+'''
+
+class Strategy():
+    @abstractmethod
+    def execute(self, a, b):
         pass
 
-class CityRoute(Route):
-    def get_path(self, a, b):
-        return {1: a, 2: b}
+class ValidateStrategy(Strategy):
+    def execute(self, a, b):
+        a = str(a)
+        b = str(b)
+        reg = "^\d{1,2}"
+        a = re.search(reg, a)
+        b = re.search(reg, b)
+        if a and b:
+            return True
+        else:
+            return False
 
-class Gps():
-    _route: Route = None
+class AddStrategy(Strategy):
+    def execute(self, a, b):
+        a = int(a)
+        b = int(b)
+        return a + b
 
-    def __init__(self, route: Route):
-        self._route = route
+class SubStrategy(Strategy):
+    def execute(self, a, b):
+        a = int(a)
+        b = int(b)
+        return a - b
 
-    def get_route(self, a, b):
-        print(self._route.get_path(a, b))
+class Context():
+    _strategy: Strategy
 
-route = CityRoute()
-gps = Gps(route)
-gps.get_route('Goiânia', 'Rio de Janeiro')
+    def __init__(self, strategy: Strategy):
+        self._strategy = strategy
+    
+    def set_strategy(self, strategy):
+        self._strategy = strategy
+    
+    def execute(self, a, b):
+        return self._strategy.execute(a, b)
+
+class App():
+    def example(self):
+        a = input('a: ')
+        b = input('b: ')
+        validate = ValidateStrategy()
+        context = Context(validate)
+        valid = context.execute(a, b)
+        
+        if valid:
+            add = AddStrategy()
+            context.set_strategy(add)
+            result = context.execute(a, b)
+            print("Result is {}".format(result))
+        else:
+            print("The input must be only integer numbers")
+
+app = App()
+app.example()
