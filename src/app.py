c3f0833141ac5e77a14474ce5c0a3f5e819de22c
Gustavo <gustavocop14dos@gmail.com>
2023-11-15 12:25:36 -0300

    - created project classes

--- src/app.py
+++ src/app.py
@@ -1,6 +1,12 @@
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, Float
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__FILE__))
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, 'planets.db')
+
+db = SQLAlchemy(app)
 
 @app.route('/check')
 def check():
@@ -26,6 +32,22 @@ def url_variables(name = None, age = 0):
         "name": name,
         "age": age
     }), 202
+
+# database models
+class User(db.Model):
+    __table__ = 'users'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    email = Column(String, unique=True)
+    password = Column(String)
+
+class Planet(db.Model):
+    __table__ = 'planets'
+    planet_id = Column(Integer, primary_key=True)
+    planet_name = Column(String)
+    planet_type = Column(String)
+    mass = Column(Float)
+    distance = Column(Float)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
